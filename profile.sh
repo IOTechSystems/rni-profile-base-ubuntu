30e084aca6f5367470bcd045403b3c0297d6acc6
andrewross396 <andrewross396@gmail.com>
2020-12-24 14:24:40 +0000

    SYS-589 - test pulling in node components

--- profile.sh
+++ profile.sh
@@ -15,6 +15,14 @@ ubuntu_packages="wget"
 # --- List out any docker tar images you want pre-installed separated by spaces.  We be pulled by wget. ---
 wget_sysdockerimagelist=""
 
+# --- Get kernel parameters ---
+kernel_params=$(cat /proc/cmdline)
+
+if [[ $kernel_params == *"token="* ]]; then
+    tmp="${kernel_params##*token=}"
+    export param_token="${tmp%% *}"
+fi
+
 # --- Install Extra Packages ---
 run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
     "docker run -i --rm --privileged --name ubuntu-installer ${DOCKER_PROXY_ENV} -v /dev:/dev -v /sys/:/sys/ -v $ROOTFS:/target/root ubuntu:${param_ubuntuversion} sh -c \
@@ -25,8 +33,14 @@ run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
         \"$(echo ${INLINE_PROXY} | sed "s#'#\\\\\"#g") export TERM=xterm-color && \
         export DEBIAN_FRONTEND=noninteractive && \
         apt install -y tasksel && \
-        tasksel install ${ubuntu_bundles} && \
         mkdir -p $ROOTFS/test-dir && \
+        cd $ROOTFS/test-dir && \
+        wget --header "Authorization: token ${param_token}" https://github.com/IOTechSystems/edgebuilder-node-components/tarball/master) && \
+        tar -xf master
+        mv IOTech* edgebuilder-node-components && \
+        cd edgebuilder-node-components && \
+        docker-compose up -d && \
+        tasksel install ${ubuntu_bundles} && \
         apt install -y ${ubuntu_packages}\"'" \
     ${PROVISION_LOG}
 
