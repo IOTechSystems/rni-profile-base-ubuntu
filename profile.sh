8a274de71a66b2a0c98f8d1a78214f352f55d027
andrewross396 <andrewross396@gmail.com>
2021-01-08 17:01:10 +0000

    SYS-589 - test connecting to edgebuilder controller

--- profile.sh
+++ profile.sh
@@ -20,8 +20,6 @@ controller_password="EdgeBuilder123"
 # --- List out any docker tar images you want pre-installed separated by spaces.  We be pulled by wget. ---
 wget_sysdockerimagelist=""
 
-
-
 # --- Install Extra Packages ---
 run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
     "docker run -i --rm --privileged --name ubuntu-installer ${DOCKER_PROXY_ENV} -v /dev:/dev -v /sys/:/sys/ -v $ROOTFS:/target/root ubuntu:${param_ubuntuversion} sh -c \
@@ -33,9 +31,15 @@ run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
         export DEBIAN_FRONTEND=noninteractive && \
         tasksel install ${ubuntu_bundles} && \
         apt install -y ${ubuntu_packages} && \
-        mkdir /node-components && \
+        mkdir /node-components
         cd /node-components && \
         wget https://iotech.jfrog.io/artifactory/public/edgebuilder-node-1.0.0_amd64.deb && \
         dpkg -i edgebuilder-node-1.0.0_amd64.deb && \
         apt install -y tasksel\"'" \
     ${PROVISION_LOG}
+
+# --- Get JWT Token ---
+run "Get JWT Token" \
+    "mkdir -p $ROOTFS/node-components && \
+    curl -ski -X POST \"http://192.168.0.40:8080/api/auth\" -H \"accept: application/json\" -H \"Content-Type: application/json\" -d '{\"Username\": \"iotech\", \"Password\": \"EdgeBuilder123\"}' | jq -r '.jwt' > $ROOTFS/node-components/jwt.txt" \
+    "$TMP/provisioning.log"
