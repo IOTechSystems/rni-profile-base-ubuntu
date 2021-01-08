60c3d9e4357282614d92f6615cca2c5cb1aadf81
andrewross396 <andrewross396@gmail.com>
2021-01-08 18:54:44 +0000

    SYS-589 - furhter test connecting to edgebuilder controller

--- profile.sh
+++ profile.sh
@@ -46,5 +46,5 @@ run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
 # --- Get JWT Token ---
 run "Get JWT Token" \
     "mkdir -p $ROOTFS/controller && \
-    curl -ski -X POST \"http://192.168.0.40:8080/api/auth\" -H \"accept: application/json\" -H \"Content-Type: application/json\" -d '{\"Username\": \"iotech\", \"Password\": \"EdgeBuilder123\"}' | jq -r '.jwt' > $ROOTFS/controller/jwt.txt" \
+    curl -sk -X POST \"http://192.168.0.40:8080/api/auth\" -H \"accept: application/json\" -H \"Content-Type: application/json\" -d '{\"Username\": \"iotech\", \"Password\": \"EdgeBuilder123\"}' | jq -r '.jwt' > $ROOTFS/controller/jwt.txt" \
     "$TMP/provisioning.log"
