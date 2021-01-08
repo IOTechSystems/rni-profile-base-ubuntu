b2f1638c5cd1b768264d35553f7af10f1a731e9b
andrewross396 <andrewross396@gmail.com>
2021-01-08 17:14:47 +0000

    SYS-589 - test connecting to edgebuilder controller

--- profile.sh
+++ profile.sh
@@ -40,6 +40,5 @@ run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
 
 # --- Get JWT Token ---
 run "Get JWT Token" \
-    "mkdir -p $ROOTFS/node-components && \
-    curl -ski -X POST \"http://192.168.0.40:8080/api/auth\" -H \"accept: application/json\" -H \"Content-Type: application/json\" -d '{\"Username\": \"iotech\", \"Password\": \"EdgeBuilder123\"}' | jq -r '.jwt' > $ROOTFS/node-components/jwt.txt" \
+    "curl -ski -X POST \"http://192.168.0.40:8080/api/auth\" -H \"accept: application/json\" -H \"Content-Type: application/json\" -d '{\"Username\": \"iotech\", \"Password\": \"EdgeBuilder123\"}' | jq -r '.jwt' > $ROOTFS/node-components/jwt.txt" \
     "$TMP/provisioning.log"
